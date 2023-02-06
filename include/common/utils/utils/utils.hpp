26a60460927d49907100587d714729ebddad2b1c
malatindez <axenance@gmail.com>
2023-02-06 13:02:26 +0200

    initial commit

---
+++ include/common/utils/utils/utils.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "core/math.hpp"
+#include "include/library-pch.hpp"
+#include "converter.hpp"
+#include "debug.hpp"
+#include "file-watcher.hpp"
+#include "measurer.hpp"
+#include "string.hpp"
+#include "template.hpp"
+#include "timer.hpp"
+#include "stl-helpers.hpp"
+#include "macro.hpp"
+#include "common-structures.hpp"
---
+++ include/win-debug.hpp
@@ -0,0 +1,8 @@
+#include "include/library-pch.hpp"
+#include "win.hpp"
+#include <malloc.h>
+namespace utils::debug
+{
+    // works only if there's no debugger attached to the process
+    void RedirectOutputDebugString(std::function<void(std::string_view)> const &);
+}
