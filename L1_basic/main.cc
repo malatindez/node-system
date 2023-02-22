f67c6f18b6484c02fa8d0e9fccf56702dc8bb4d0
malatindez <axenance@gmail.com>
2023-02-22 17:20:02 +0200

    update

--- L1_basic/main.cc
+++ L1_basic/main.cc
@@ -1,4 +1,32 @@
-int main()
-{
-    return 0;   
+#include <iostream>
+#include <boost/asio.hpp>
+#include <iostream>
+class udp_client {
+public:
+    udp_client(const std::string& host, int port) : socket(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 0)), endpoint(boost::asio::ip::address::from_string(host), port) {}
+
+    void send(const std::string& message) {
+        socket.async_send_to(boost::asio::buffer(message), endpoint,
+            [this, &message](boost::system::error_code ec, std::size_t bytes_sent) {
+                if (!ec && bytes_sent > 0) {
+                    std::cout << "Sent " << bytes_sent << " bytes: " << message << std::endl;
+                }
+            });
+    }
+
+    boost::asio::io_service io_service;
+    boost::asio::ip::udp::socket socket;
+    boost::asio::ip::udp::endpoint endpoint;
+};
+
+int main() {
+    udp_client client("127.0.0.1", 1234);
+    std::string message;
+    std::cout << "Write your secret message: ";
+    std::cin >> message;
+    client.send(message);
+
+    client.io_service.run();
+
+    return 0;
 }
--- L2_core/main.cc
+++ L2_core/main.cc
@@ -1,4 +1,33 @@
-int main()
-{
-    return 0;   
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+
+class udp_receiver {
+public:
+    udp_receiver(int port) : socket(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port)) {
+        start_receive();
+    }
+
+    void start_receive() {
+        socket.async_receive_from(boost::asio::buffer(recv_buffer), remote_endpoint,
+            [this](boost::system::error_code ec, std::size_t bytes_recvd) {
+                if (!ec && bytes_recvd > 0) {
+                    std::cout << "Received " << bytes_recvd << " bytes: " << std::string(recv_buffer.data(), bytes_recvd) << std::endl;
+                }
+        start_receive();
+            });
+    }
+
+    boost::asio::io_service io_service;
+    boost::asio::ip::udp::socket socket;
+    boost::array<char, 1024> recv_buffer;
+    boost::asio::ip::udp::endpoint remote_endpoint;
+};
+
+int main() {
+    udp_receiver receiver(1234);
+    receiver.start_receive();
+    receiver.io_service.run();
+
+    return 0;
 }
